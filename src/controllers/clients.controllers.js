0bbdb59e3bc917ddffc95ce6bcf52f56a1cf8308
estefa99 <estefanobugari@gmail.com>
2023-10-10 16:21:24 -0300

    CRUD clientes terminado

--- src/controllers/clients.controllers.js
+++ src/controllers/clients.controllers.js
@@ -1,5 +1,5 @@
 import data from '../models/data.json' assert { type: "json" };
-// import {fs} from 'fs';
+import * as fs from 'node:fs';
 
 
 const clients = data.clients;
@@ -14,26 +14,79 @@ export const getClientById = (req, res) => {
     res.json(client)
 }
 
-export const createClient = (req, res) => {
-    const {id, client_name} = req.body;
-    const newClient = {id, client_name};
+export const createClient = async (req, res) => {
+    const { id, client_name } = req.body;
 
-    // fs.readFile('data.json', 'utf8', (err, data) => {
-    //   if (err) {
-    //     console.error('Error al leer el archivo JSON:', err);
-    //     return;
-    //   }
-    //   const json = JSON.parse(data);
-    //   // Ahora tienes el contenido del archivo JSON en el objeto 'json'.
-    // });
+    if ([id, client_name].includes("")) {
+        return res.status(403).send("Campos incompletos");
+    }
+
+    try {
+        //Traemos la data del archivo json y le agregamos en la variable el nuevo cliente que vino del front.
+        const data = await fs.promises.readFile('./src/models/data.json');
+        const jsonData = JSON.parse(data);
+        jsonData.clients.push(req.body);
     
-    // clients.push(newClient);
+        //Modifica el json con el cliente nuevo agregado.
+        await fs.promises.writeFile("./src/models/data.json", JSON.stringify(jsonData, null, 2), 'utf-8')
+        return res.send("Cliente creado exitosamente");
+    
+      } catch (error) {
+        return res.status(403).send(`Hubo un error: ${error}`)
+      }
 }
 
-export const updateClient = (req, res) => {
-    return null;
+export const updateClient = async (req, res) => {
+    const { id } = req.params;
+    const { client_name } = req.body;
+
+    if ([id, client_name].includes("")) {
+        return res.status(403).send("Campos incompletos");
+    }
+
+    try {
+        //Traemos la data del archivo json y le agregamos en la variable el nuevo client que vino del front.
+        const data = await fs.promises.readFile('./src/models/data.json');
+        const jsonData = JSON.parse(data);
+
+        // Modificamos el cliente comparando el ID del arreglo del json con el del front
+        jsonData.clients = jsonData.clients.map(client => {
+            if (client.id == id) {
+            client.id = Number(id),
+            client.client_name = client_name;
+            }
+            return client;
+        });
+    
+        //Modifica el json con el cliente nuevo agregado.
+        await fs.promises.writeFile("./src/models/data.json", JSON.stringify(jsonData, null, 2), 'utf-8')
+        return res.send("Cliente editado exitosamente");
+    
+      } catch (error) {
+        return res.status(403).send(`Hubo un error: ${error}`)
+      }
 }
 
-export const deleteClient = (req, res) => {
-    return null;
+export const deleteClient = async (req, res) => {
+    const { id } = req.params;
+
+    if (!id) {
+        return res.status(403).send("No se envió ningún cliente");
+    }
+
+    try {
+        //Traemos la data del archivo json
+        const data = await fs.promises.readFile('./src/models/data.json');
+        const jsonData = JSON.parse(data);
+    
+        //Filtramos los clientes que tengan el ID diferente al que viene del front
+        jsonData.clients = jsonData.clients.filter(client => client.id != id);
+    
+        //Modifica el archivo json con el cliente nuevo editado.
+        await fs.promises.writeFile("./src/models/data.json", JSON.stringify(jsonData, null, 2), 'utf-8')
+        return res.send("Cliente eliminado exitosamente");
+    
+      } catch (error) {
+        return res.status(403).send(`Hubo un error: ${error}`)
+      }
 }
